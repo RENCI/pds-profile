202cb5d804c283b72e2e05c584709ba31d3004eb
Hao Xu <xuh@cs.unc.edu>
2019-10-29 22:28:11 -0400

    update

--- api/__init__.py
+++ api/__init__.py
@@ -6,14 +6,14 @@ pds_host = os.environ["PDS_HOST"]
 pds_port = os.environ["PDS_PORT"]
 pds_url_base = f"http://{pds_host}:{pds_port}/plugin"
 
-def profile(patient_id, model, model_plugin_interface, phenotype_mapping_plugin_interface, data_provider_plugin_interface, timestamp):
-    resp1 = requests.get(f"{pds_url_base}/{model_plugin_interface}/clinical_feature_variables?model={model}")
+def profile(patient_id, model, model_plugin_id, phenotype_mapping_plugin_id, data_provider_plugin_id, timestamp):
+    resp1 = requests.get(f"{pds_url_base}/{model_plugin_id}/clinical_feature_variables?model={model}")
     clinical_feature_variable_objects = resp1.json()
     profile = []
     for clinical_feature_variable_object in clinical_feature_variable_objects:
         clinical_feature_variable = clinical_feature_variable_object["clinical_feature_variable"]
         description = clinical_feature_variable_object["description"]
-        url = f"{pds_url_base}/{phenotype_mapping_plugin_interface}/mapping?patient_id={patient_id}&clinical_feature_variable={clinical_feature_variable}&data_provider_plugin_interface={data_provider_plugin_interface}&timestamp={timestamp}"
+        url = f"{pds_url_base}/{phenotype_mapping_plugin_id}/mapping?patient_id={patient_id}&clinical_feature_variable={clinical_feature_variable}&data_provider_plugin_id={data_provider_plugin_id}&timestamp={timestamp}"
         print(f"url = {url}")
         resp2 = requests.get(url)
         value_object = resp2.json()
